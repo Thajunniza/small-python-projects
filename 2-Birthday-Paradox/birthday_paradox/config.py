c13ff3c52f47550951e4038d684e150138aa02dd
A <thajunniza.m.a@gmail.com>
2026-02-01 14:38:13 +0000

    chore: initial project structure for Birthday Paradox

---
+++ 2-Birthday-Paradox/birthday_paradox/config.py
@@ -0,0 +1,10 @@
+"""
+Default configuration values for the Birthday Paradox simulation.
+
+These values represent assumptions and sensible defaults.
+They can be overridden by function arguments elsewhere in the package.
+"""
+
+MAX_GROUP_SIZE = 100    # Max allowed people in simulation
+DEFAULT_DAYS_IN_YEAR = 365  # We are considering Non-Leap Year
+DEFAULT_SIMULATION_COUNT = 100_000   #We are runnin 100,000 Simulations
